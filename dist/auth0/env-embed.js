1b43dbbc93a8a24c3f9e633f00cacca6975a2021
IrinaSklyarova <irina060981@yandex.ru>
2018-12-28 12:14:09 +1000

    define packages

---
+++ dist/auth0/env-embed.js
@@ -0,0 +1,5 @@
+const auth0Env = {
+  CLIENT_ID: 'iT75HkBHThA4QdFwFoZRofLC41vVyvAt',
+  DOMAIN: 'alpheios.auth0.com',
+  ENDPOINT: 'https://k9z4qmom0i.execute-api.us-east-2.amazonaws.com/prod/notes'
+}
---
+++ dist/auth0/env-webext.js
@@ -0,0 +1,5 @@
+const auth0Env = {
+  AUTH0_DOMAIN: 'alpheios.auth0.com',
+  AUTH0_CLIENT_ID: 'iT75HkBHThA4QdFwFoZRofLC41vVyvAt',
+  ENDPOINT: 'https://k9z4qmom0i.execute-api.us-east-2.amazonaws.com/prod/notes'
+}
