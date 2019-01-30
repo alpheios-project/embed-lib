7f738994dda911c902b4c57e91831c63c21f3c8f
Bridget Almas <balmsa@gmail.com>
2019-01-30 14:25:10 -0500

    add support for test id for auth

---
+++ dist/auth0/env-embed-dev.js
@@ -0,0 +1,8 @@
+const auth0Env = {
+  CLIENT_ID: 'iT75HkBHThA4QdFwFoZRofLC41vVyvAt',
+  DOMAIN: 'alpheios.auth0.com',
+  ENDPOINT: 'https://w2tfh159s2.execute-api.us-east-2.amazonaws.com/prod/words',
+  AUDIENCE: 'alpheios.net:apis',
+  LOGOUT_URL: 'https://alpheios.net/pages/logout',
+  TEST_ID: 'alpheiosMockUserIdFqrIYqYNmB'
+}
